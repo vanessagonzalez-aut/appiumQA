71c3f80299207b3b76257e1fa51843f2616f2695
vane0912 <vanessagpro5@gmail.com>
2026-07-12 19:17:14 -0600

    change email

--- urls.js
+++ urls.js
@@ -1,4 +1,4 @@
 const branch = 'https://deploy-20260713--6940e2f0.visachinaonline.com/'
-const email = 'test203@mailinator.com'
+const email = 'test205@mailinator.com'
 
 module.exports = {branch, email}
