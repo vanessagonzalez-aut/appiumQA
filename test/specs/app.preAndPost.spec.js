62079a924b3e510f94e1ea6f0ebc7d08e4a7b982
vane0912 <vanessagpro5@gmail.com>
2026-07-22 15:15:43 -0600

    move travelers

--- test/specs/app.preAndPost.spec.js
+++ test/specs/app.preAndPost.spec.js
@@ -31,16 +31,16 @@ describe('Travel doc pre and post payment are working correctly', () => {
     }else{
       await mainFunctions.nonExistingAccountInfo(driver, 'Upload file')
     }
-    await driver.$('~Save and Continue').click()
+    await driver.$('~Travelers').waitForDisplayed()
+    await driver.$('~Save and Continue').click() 
     try{
       await driver.$('~Choose your processing time').waitForDisplayed({timeout: 3000})
       await driver.$('~Save and Continue').click()
     }catch{
 
     }
-    await driver.$('~Travelers').waitForDisplayed()
-    await driver.$('~Save and Continue').click() 
     await mainFunctions.checkDuplicateBanner(driver)
+    
     await driver.$('~Order Review').waitForDisplayed()
 
     if (existingAccount === false){
--- urls.js
+++ urls.js
@@ -1,4 +1,4 @@
 const branch = 'https://deploy-20260723--cb0aabd2.visachinaonline.com/'
-const email = 'tes46@mailinator.com'
+const email = 'tes47@mailinator.com'
 
 module.exports = {branch, email}
